8cdf32b0a84fa82187002a4cafacd84b7f23bf42
romka201616 <mironovkilleer@gmail.com>
2023-11-18 18:49:21 +0300

    commit

--- Scripts/final_screen.gd
+++ Scripts/final_screen.gd
@@ -10,13 +10,17 @@ func _ready():
 		get_node("UI/WinnerScore").text = "Счёт: " + str(Global.teams[0].score)
 		for i in range(2, 7):
 			get_node("UI/TeamsContainer/Team" + str(i)).hide()
-			
+		
+		Global.teams.sort_custom(custom_sort)
+		
 		for i in range(2, Global.teams_amount + 1):
 			get_node("UI/TeamsContainer/Team" + str(i) + "/Name").text = Global.teams[i-1].team_name
 			get_node("UI/TeamsContainer/Team" + str(i) + "/Score").text = str(Global.teams[i-1].score)
 			get_node("UI/TeamsContainer/Team" + str(i)).show()
 
-
+func custom_sort(a : Team, b : Team):
+	if typeof(a.score) == typeof(b.score):
+		return a.score > b.score;
 
 func _on_new_game_pressed():
 	var next_scene = load("res://Scenes/menu.tscn")
